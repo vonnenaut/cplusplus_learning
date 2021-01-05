ad0da5f97e7c67c7daf5e36444208ed481bc2f7f
vonnenaut <danielashcom@gmail.com>
2021-01-05 20:58:22 +0700

    u/d files

---
+++ cpp_crash_course/cmake-build-debug/CMakeFiles/cpp_crash_course.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_crash_course.dir/main.cpp.o"
+  "cpp_crash_course"
+  "cpp_crash_course.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_crash_course.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cpp_crash_course/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    bool isTodaySunny = false;
+
+    isTodaySunny ? cout << "Go to the park" : cout << "Take an umbrella";
+
+
+    return 0;
+}
