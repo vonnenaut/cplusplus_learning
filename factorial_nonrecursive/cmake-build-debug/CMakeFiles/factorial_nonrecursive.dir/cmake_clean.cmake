519586f053e25768a4fc6dff50eb6cc364ea8d0c
vonnenaut <danielashcom@gmail.com>
2021-01-10 19:02:13 +0700

    automated sync commit for cplusplus_learning

---
+++ factorial_nonrecursive/cmake-build-debug/CMakeFiles/factorial_nonrecursive.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/factorial_nonrecursive.dir/main.cpp.o"
+  "factorial_nonrecursive"
+  "factorial_nonrecursive.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/factorial_nonrecursive.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ factorial_nonrecursive/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "BigIntegerLibrary.hh"
+
+unsigned BigInteger factorial(unsigned BigInteger value);
+
+int main() {
+    unsigned BigInteger value = 5;
+    unsigned BigInteger fac = factorial(value);
+
+    printf("%d! is %d\n", value, fac);
+
+    value = 2;
+    printf("%d! is %d\n", value, factorial(value));
+
+    value = 12;
+    printf("%d! is %d\n", value, factorial(value));
+
+    value = 16;
+    printf("%d! is %d\n", value, factorial(value));
+
+    return 0;
+}
+
+unsigned long factorial(unsigned long value) {
+    if (value < 1) {
+        printf("Error:  Invalid input (< 1)");
+        return -1;
+    } else if (value == 1) {
+        return 1;
+    }
+
+    unsigned long factorial = 1;
+
+    for(unsigned long current = value; current > 0; current--) {
+        factorial *= current;
+    }
+
+    return factorial;
+}
